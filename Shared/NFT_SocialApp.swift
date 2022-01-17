94812548ed6216e6b60a248db625c7083fcc93c1
hosituan <hosituan.work@gmail.com>
2022-01-17 17:41:27 +0700

    Initial Commit

---
+++ Shared/NFT_SocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFT_SocialApp.swift
+//  Shared
+//
+//  Created by HoSiTuan on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct NFT_SocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
